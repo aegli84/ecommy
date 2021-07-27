1d2ef5c4f95bac6c68be62ca3965e7aad2a2701b
aegli84 <egli.andreea@yahoo.com>
2021-07-27 22:46:11 +0200

    v1

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+import { ShoppingCart } from '@material-ui/icons'
+
+const Navbar = () => {
+    return (
+        <>
+            <AppBar/>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/components/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Grid  from '@material-ui/core/Grid'
+import Product from './Product/Product'
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running Shoes', price:'$5', image: 'https://www.asos.com/adidas-originals/adidas-originals-essentials-t-shirt-in-yellow/prd/23572675?colourwayid=60494685&SearchQuery=adidas+t+shirt'},
+    { id: 2, name: 'Tshirt', description: 'Adidas', price: '$10', image:'https://www.asos.com/adidas-originals/adidas-originals-forum-low-collegiate-trainers-in-white/prd/23549098?colourwayid=60492920&SearchQuery=adidas'},
+]
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid  key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products;
