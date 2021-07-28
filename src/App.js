c916b7e6c82beb3a520b40ac20f4fb21015c829f
aegli84 <egli.andreea@yahoo.com>
2021-07-28 20:17:20 +0200

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import Products from './components/Products'
+import React, { useState, useEffect }from 'react'
+import { commerce } from './lib/commerce'
+import { Products, Navbar  } from './components'
 
 const App = () => {
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
     return (
         <div>
+            <Navbar/>
             <Products/>
         </div>
     )
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
 import useStyles from './styles'
@@ -7,21 +6,21 @@ const Product = ({ product }) => {
     const classes = useStyles();
     return (
         <Card className={classes.root}>
-            <CardMedia classesName={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
-                    <Typography variant='h5' gutterBottom>
+                    <Typography  variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
-                    <Typography variant='h5'>
+                    <Typography  variant="h5">
                         {product.price}
                     </Typography>
                 </div>
                 <Typography variant='body2' color='textSecondary'>{product.description}</Typography>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label='Add to Cart'>
-                    <AddShoppingCart/>
+                <IconButton aria-label="Add to Cart" >
+                    <AddShoppingCart />
                 </IconButton>
             </CardActions>
         </Card>
